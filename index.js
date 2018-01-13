75cd6fdf84cb5b569a9613f6c1f3cfd74ac6ff00
Lisa Lee <snickers495@gmail.com>
2018-01-13 20:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
  const returnFirstTwoDrivers = function(arr) {
-   return [arr[0], arr[1]]
+   return [arr[0], arr[1]];
  }
  const returnLastTwoDrivers() = function(arr) {
-   return [arr[-2], arr[-1]]
+   return [arr[-2], arr[-1]];
  }
