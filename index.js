838e50676b66d3417301ffdb46c390f87d7d8e0b
Lisa Lee <snickers495@gmail.com>
2018-01-13 20:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  const returnFirstTwoDrivers = function(arr) {
    return [arr[0], arr[1]];
  }
- const returnLastTwoDrivers() = function(arr) {
+ const returnLastTwoDrivers = function(arr) {
    return [arr[(arr.length - 2)], arr[(arr.length - 1)]];
  }
